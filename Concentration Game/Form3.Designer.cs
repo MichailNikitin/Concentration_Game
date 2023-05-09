9a9c66250781c04d41763c415b19a40531e8d860
Miniki <miniki2002@yandex.ua>
2023-05-09 23:00:18 +0500

    test

---
+++ Concentration Game/Form3.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Concentration_Game
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(96, 48);
+            this.startBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(88, 30);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Начать";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 361);
+            this.Controls.Add(this.startBtn);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBtn;
+    }
+}
---
+++ Concentration Game/Form4.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Concentration_Game
+{
+    public class SelectedPairs
+    {
+        public string Card1Id = "";
+        public string Card2Id = "";
+    }
+    public partial class Form4 : Form
+    {
+        SelectedPairs SelectedPair = new SelectedPairs();
+        public Form4()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 16; i++)
+            {
+                Button btn = new Button();
+
+                btn.Text = $"Button {i}";
+                btn.Height = 200;
+                btn.Width= 100;
+                //btn.Image = new Bitmap($"images/{i}.jpg");
+                btn.ImageAlign = ContentAlignment.MiddleCenter;
+                btn.BackgroundImage = new Bitmap("images/back.jpg");
+                btn.Tag = i%8;
+
+                btn.BackgroundImageLayout = ImageLayout.Stretch;
+
+                btn.Click += Btn_Click;
+
+                flowLayoutPanel1.Controls.Add(btn);
+            }
+
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = ((Button)sender);
+            string text = btn.Tag.ToString();
+
+            if (flowLayoutPanel1.Controls.Count == 0)
+            {
+                
+            }
+            if (string.IsNullOrWhiteSpace(SelectedPair.Card1Id))
+            {
+                SelectedPair.Card1Id = text;
+            }
+            else
+            {
+                SelectedPair.Card2Id = text;
+            }
+
+            if (SelectedPair.Card1Id == SelectedPair.Card2Id)
+            {
+                Thread.Sleep(1000);
+                flowLayoutPanel1.Controls.Remove(flowLayoutPanel1.Controls.Cast<Button>().First(x => x.Tag.ToString() == SelectedPair.Card1Id));
+                flowLayoutPanel1.Controls.Remove(flowLayoutPanel1.Controls.Cast<Button>().First(x => x.Tag.ToString() == SelectedPair.Card2Id));
+            }
+
+            btn.BackgroundImage = new Bitmap($"images/{text}.PNG");
+            //flowLayoutPanel1.Controls.Clear();
+            //MessageBox.Show(text);
+        }
+    }
+}
